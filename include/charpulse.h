a0088eea2c7206f1b41d237a4c98babdbab6ca20
Sreeraj S Kurup <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2025-12-06 00:04:08 +0530

    Remove CP_SET_MAX_SIZE ioctl from charpulse.h

--- include/charpulse.h
+++ include/charpulse.h
@@ -21,7 +21,6 @@
 
 #define CP_CLEAR_BUFFER    _IO(CHARPULSE_IOC_MAGIC, 1)
 #define CP_GET_STATS       _IOR(CHARPULSE_IOC_MAGIC, 2, struct cp_stats)
-#define CP_SET_MAX_SIZE    _IOW(CHARPULSE_IOC_MAGIC, 3, size_t)
 #define CP_GET_BUFFER_USAGE  _IOR(CHARPULSE_IOC_MAGIC, 4, char*)
 
 struct cp_stats {
